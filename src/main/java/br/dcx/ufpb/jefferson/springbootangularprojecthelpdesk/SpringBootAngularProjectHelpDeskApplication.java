49d646e799d8dbac827725271fe8fd94bccb1f81
Jefferson Bezerra <jeffebezerram13@gmail.com>
2025-07-06 11:34:11 -0300

    first commit

---
+++ src/main/java/br/dcx/ufpb/jefferson/springbootangularprojecthelpdesk/SpringBootAngularProjectHelpDeskApplication.java
@@ -0,0 +1,13 @@
+package br.dcx.ufpb.jefferson.springbootangularprojecthelpdesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAngularProjectHelpDeskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAngularProjectHelpDeskApplication.class, args);
+    }
+
+}
