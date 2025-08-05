1b6dea6dd4cd5a17ef30e6ae1e506cb504e362e8
Jefferson Bezerra <jeffebezerram13@gmail.com>
2025-08-05 18:07:26 -0300

    feat: UserSS contract methods

---
+++ src/main/java/br/dcx/ufpb/jefferson/springbootangularprojecthelpdesk/security/UserSS.java
@@ -0,0 +1,48 @@
+package br.dcx.ufpb.jefferson.springbootangularprojecthelpdesk.security;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.Collection;
+import java.util.List;
+
+public class UserSS implements UserDetails {
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return "";
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
