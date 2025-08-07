8d057a917f77f69687dc7d45e289dc66e7b39f60
Jefferson Bezerra <jeffebezerram13@gmail.com>
2025-08-06 21:42:45 -0300

    feat: instantiation date base service created

--- src/main/java/br/dcx/ufpb/jefferson/springbootangularprojecthelpdesk/config/InstantiationDbDev.java
+++ src/main/java/br/dcx/ufpb/jefferson/springbootangularprojecthelpdesk/config/InstantiationDbDev.java
@@ -1,54 +1,22 @@
 package br.dcx.ufpb.jefferson.springbootangularprojecthelpdesk.config;
 
-import br.dcx.ufpb.jefferson.springbootangularprojecthelpdesk.domain.entities.Call;
-import br.dcx.ufpb.jefferson.springbootangularprojecthelpdesk.domain.entities.Client;
-import br.dcx.ufpb.jefferson.springbootangularprojecthelpdesk.domain.entities.Technical;
-import br.dcx.ufpb.jefferson.springbootangularprojecthelpdesk.domain.entities.enums.Priority;
-import br.dcx.ufpb.jefferson.springbootangularprojecthelpdesk.domain.entities.enums.Status;
-import br.dcx.ufpb.jefferson.springbootangularprojecthelpdesk.repositories.CallRepository;
-import br.dcx.ufpb.jefferson.springbootangularprojecthelpdesk.repositories.ClientRepository;
-import br.dcx.ufpb.jefferson.springbootangularprojecthelpdesk.repositories.PersonRepository;
-import br.dcx.ufpb.jefferson.springbootangularprojecthelpdesk.repositories.TechnicalRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import br.dcx.ufpb.jefferson.springbootangularprojecthelpdesk.services.DatabaseSeederService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile("dev")
+@org.springframework.context.annotation.Profile("dev") // Evita conflito com o profile principal da aplicação
 public class InstantiationDbDev implements CommandLineRunner {
-    @Autowired
-    private CallRepository callRepository;
 
-    @Autowired
-    private ClientRepository clientRepository;
+    private final DatabaseSeederService databaseSeederService;
 
-    @Autowired
-    private PersonRepository personRepository;
-
-    @Autowired
-    private TechnicalRepository technicalRepository;
+    public InstantiationDbDev(DatabaseSeederService databaseSeederService) {
+        this.databaseSeederService = databaseSeederService;
+    }
 
     @Override
     public void run(String... args) throws Exception {
-        cleanDb();
-
-        Technical tech1 = new Technical(null,"Bob Green","906.812.820-53","bob@gmail.com","123");
-        tech1.addProfile(br.dcx.ufpb.jefferson.springbootangularprojecthelpdesk.domain.entities.enums.Profile.ADMIN);
-
-        Client cli1 = new Client(null,"Ana Pink", "859.288.630-98","ana@gmail.com,","123");
-
-        Call c1 = new Call(null, Priority.MEDIUM, Status.IN_PROGRESS,"Chamado 01","Primeiro chamado",tech1,cli1);
-
-        technicalRepository.save(tech1);
-        clientRepository.save(cli1);
-        callRepository.save(c1);
-    }
-
-    private void cleanDb() {
-        callRepository.deleteAll();
-        clientRepository.deleteAll();
-        personRepository.deleteAll();
-        technicalRepository.deleteAll();
+        databaseSeederService.populateDatabase();
     }
 }
--- src/main/java/br/dcx/ufpb/jefferson/springbootangularprojecthelpdesk/config/InstantiationDbTest.java
+++ src/main/java/br/dcx/ufpb/jefferson/springbootangularprojecthelpdesk/config/InstantiationDbTest.java
@@ -10,6 +10,7 @@ import br.dcx.ufpb.jefferson.springbootangularprojecthelpdesk.repositories.CallR
 import br.dcx.ufpb.jefferson.springbootangularprojecthelpdesk.repositories.ClientRepository;
 import br.dcx.ufpb.jefferson.springbootangularprojecthelpdesk.repositories.PersonRepository;
 import br.dcx.ufpb.jefferson.springbootangularprojecthelpdesk.repositories.TechnicalRepository;
+import br.dcx.ufpb.jefferson.springbootangularprojecthelpdesk.services.DatabaseSeederService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
@@ -25,59 +26,14 @@ import java.util.Arrays;
 @org.springframework.context.annotation.Profile("test") // Evita conflito com o profile principal da aplicação
 public class InstantiationDbTest implements CommandLineRunner {
 
-    @Autowired
-    private CallRepository callRepository;
+    private final DatabaseSeederService databaseSeederService;
 
-    @Autowired
-    private ClientRepository clientRepository;
-
-    @Autowired
-    private PersonRepository personRepository;
-
-    @Autowired
-    private TechnicalRepository technicalRepository;
-
-    @Autowired
-    private BCryptPasswordEncoder encoder;
-
-    /**
-     * Método executado automaticamente ao iniciar a aplicação, populando o banco com dados fictícios.
-     *
-     * @param args argumentos de inicialização (não utilizados aqui)
-     */
-    @Override
-    public void run(String... args) throws Exception {
-        clearDb(); // Remove dados anteriores para evitar duplicidade
-
-        // Criação de técnicos
-        Technical tech1 = new Technical(null, "Ana Green", "906.812.820-53", "ana@gmail.com", encoder.encode("+2Y}+uQqfH$aWin%"));
-        tech1.addProfile(Profile.ADMIN);
-
-        Technical tech2 = new Technical(null, "Bob Yellow", "917.489.110-31", "bob@gmail.com", encoder.encode("l)vg@!uB@K!:#QUW"));
-        Technical tech3 = new Technical(null, "Carl Pink", "859.288.630-98", "carl@gmail.com", encoder.encode("scZ2qYmo;@m%ENQ7"));
-
-        // Criação de clientes
-        Client cli1 = new Client(null, "Dan Pink", "630.691.410-21", "dan@gmail.com", encoder.encode("^&OR#4fhaa5gXYJG"));
-        Client cli2 = new Client(null, "Emma Black", "757.722.020-57", "emma@gmail.com", encoder.encode("C:~`F_|4t({__FG:"));
-        Client cli3 = new Client(null, "Finn Grey", "983.680.740-37", "finn@gmail.com", encoder.encode("1j]g_;.v944+N(g|"));
-
-        // Criação de chamados
-        Call c1 = new Call(null, Priority.LOW, Status.OPEN, "Chamado 01", "Primeiro chamado", tech1, cli1);
-        Call c2 = new Call(null, Priority.HIGH, Status.IN_PROGRESS, "Chamado 02", "Segundo chamado", tech1, cli2);
-
-        // Persistência no banco
-        technicalRepository.saveAll(Arrays.asList(tech1, tech2, tech3));
-        clientRepository.saveAll(Arrays.asList(cli1, cli2, cli3));
-        callRepository.saveAll(Arrays.asList(c1, c2));
+    public InstantiationDbTest(DatabaseSeederService databaseSeederService) {
+        this.databaseSeederService = databaseSeederService;
     }
 
-    /**
-     * Limpa completamente o banco de dados antes de popular com novos dados.
-     */
-    private void clearDb() {
-        callRepository.deleteAll();
-        clientRepository.deleteAll();
-        personRepository.deleteAll();
-        technicalRepository.deleteAll();
+    @Override
+    public void run(String... args) throws Exception {
+        databaseSeederService.populateDatabase();
     }
 }
